a04f31b528fde0d2fdc60fddbfc85668bed44ab5
iAMpt <trifpatricia@yahoo.com>
2023-11-07 11:10:50 +0200

    register

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes , Route } from "react-router-dom";
-import React from 'react'
-import Login from './Login';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './Login'; // Import your Login component
+import Registration from './Register'; // Import your Registration component
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login />}></Route>
-
-        </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Registration} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- client/src/Register.js
+++ client/src/Register.js
@@ -1,36 +1,31 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
-import './styles.css'; // Importă stilurile
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './styles.css';
 
-const Login = () => {
+const Registration = () => {
     return (
         <div className='d-flex justify-content-center align-items-center vh-100 pink-background'>
             <img src='logo.png' alt='Logo' className='loginPhoto' />
             <div className='bg-white p-3 rounded w-25'>
-                <h2>Login</h2>
+                <h2>Create Account</h2>
                 <form action="">
                     <div className='mb-3'>
                         <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email" placeholder='Enter Email' name='email'
-                               className='form-control rounded-0'/>
-
+                        <input type="email" placeholder='Enter Email' name='email' className='form-control rounded-0'/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password"><strong>Password</strong></label>
-                        <input type="password" placeholder='Enter password' name='password'
-                               className='form-control rounded-0' />
-
+                        <input type="password" placeholder='Enter password' name='password' className='form-control rounded-0' />
                     </div>
-                    <button type='submit' className='btn btn-success w-100'>Log in</button>
+                    <button type='submit' className='btn btn-primary w-100'>Create Account</button>
                 </form>
             </div>
             <div className='mt-3'>
-                <Link to="/register">Don't have an account? Create one</Link>
+                <Link to="/login">Already have an account? Log in</Link>
             </div>
-
         </div>
-
     )
 }
 
-export default Login;
+export default Registration;
